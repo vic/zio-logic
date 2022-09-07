40781dea6f1bbd8cd36b6a451684d23c176f7a8b
Victor Hugo Borja <vborja@apache.org>
2022-09-07 13:02:15 -0500

    LVars cache their value.

--- zio-logic/shared/src/main/scala/zkanren/internal/LTerm.scala
+++ zio-logic/shared/src/main/scala/zkanren/internal/LTerm.scala
@@ -4,8 +4,10 @@ sealed trait LTerm[+A]
 final case class LVar[+A] private[internal] (intern: Long) extends LTerm[A]
 
 final class LVal[+A] private[LVal] (thunk: () => A) extends LTerm[A] {
-  def apply(): A = thunk()
+  private[this] lazy val value = thunk()
+
+  def apply(): A = value
 }
 object LVal {
-  def apply[A](a: => A): LVal[A] = new LVal(() => a)
+  def apply[A](a: A): LVal[A] = new LVal(() => a)
 }
