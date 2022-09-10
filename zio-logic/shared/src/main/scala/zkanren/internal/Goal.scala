6bbe96cc8e89eeb2e01f79e0a82d3a7223762904
Victor Hugo Borja <vborja@apache.org>
2022-09-10 03:15:58 -0500

    Add goal negation

--- zio-logic/shared/src/main/scala/zkanren/internal/Goal.scala
+++ zio-logic/shared/src/main/scala/zkanren/internal/Goal.scala
@@ -46,12 +46,12 @@ object Goal {
   def fromChannel[R, E](ch: Chan[R, E]): Goal[R, E] = new Goal(ch)
 
   def fromFunction[R, E](f: State => Either[State, State]): Goal[R, E] =
-    fromReadConcat[R, E](ZChannel.write(_).mapOut(f))
+    fromReadLoop[R, E](ZChannel.write(_).mapOut(f))
 
   def fromZIO[R, E](f: ZIO[R with State, E, Either[State, State]]): Goal[R, E] =
-    fromReadConcat[R, E](ZChannel.write(_).mapOutZIO(s => f.provideSomeLayer[R](ZLayer.succeed(s))))
+    fromReadLoop[R, E](ZChannel.write(_).mapOutZIO(s => f.provideSomeLayer[R](ZLayer.succeed(s))))
 
-  def fromReadConcat[R, E](read: State => Chan[R, E]): Goal[R, E] = {
+  def fromReadLoop[R, E](read: State => Chan[R, E]): Goal[R, E] = {
     lazy val channel: Chan[R, E] =
       ZChannel.readWithCause(
         in = ZChannel.write(_).concatMap(read) *> channel,
@@ -61,16 +61,35 @@ object Goal {
     Goal.fromChannel(channel)
   }
 
+  // Swaps a goal exit status, making it fail if successful or the other way around.
+  def swap[R, E](g: Goal[R, E]): Goal[R, E] =
+    fromChannel(g.toChannel.mapOut(_.swap))
+
+  // Rejects all success
   def reject: Goal[Any, Nothing] = fromFunction(Left(_))
 
+  // Keeps success as it is, used mainly as fallback case in conde.
   def accept: Goal[Any, Nothing] = fromFunction(Right(_))
 
-  def neg[R, E](g: Goal[R, E]): Goal[R, E] =
-    conj(Seq(g, reject))
+  // Creates a new branch of state so that changes are not shared.
+  def branch: Goal[Any, Nothing] =
+    fromZIO[Any, Nothing](ZIO.serviceWithZIO[State](_.branch.commit.map(Right(_))))
 
+  // Performs a given goal in a temporary state branch and restores to the original state at the end.
+  def peek[R, E](g: Goal[R, E]): Goal[R, E] =
+    fromReadLoop[R, E] { savedState =>
+      val ch = conj(Seq(branch, g)).toChannel.mapOut(_.fold(_ => Left(savedState), _ => Right(savedState)))
+      ZChannel.write(savedState) >>> ch
+    }
+
+  // Negates a goal. Succeeds if goal fails in a temporary state branch.
+  def neg[R, E](g: Goal[R, E]): Goal[R, E] = peek(swap(g))
+
+  // Logical conjunction of goals. Goals are run as long as their previous one succeeds.
   def conj[R, E](goals: IterableOnce[Goal[R, E]]): Goal[R, E] =
     goals.iterator.reduceLeft[Goal[R, E]](_ pipeSuccessTo _)
 
+  // Logical disjunction of goals. Goals are run in parallel as they are independent form one another.
   def disj[R, E](goals: IterableOnce[Goal[R, E]]): Goal[R, E] = {
     lazy val chunk = Chunk.fromIterator(goals.iterator.map(_.channel))
     val ch         = ZChannel.mergeAll(ZChannel.writeChunk(chunk), n = 16)
